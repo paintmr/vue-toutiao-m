59190bec84cd56361751a26a4ba91ed35e1e0b66
chujingxl <chujingxl@gmail.com>
2021-10-02 01:08:25 +0800

    项目初始化-封装请求模块

--- postcss.config.js
+++ postcss.config.js
@@ -11,8 +11,8 @@ module.exports = {
     // 配置使用postcss-pxtorem插件
     'postcss-pxtorem': {
       // 这个rootValue基于lib-flexible的REM适配方案：把一行分为10份，每份是十分之一。所以rootValue应该设置为设计稿宽度的十分之一。大多數設計稿以iPhone6為原型，寬度為750px。
-      // 但vent建议设置为37.5，因为vent是基于375px宽度的设计稿写的。据此，使用设计稿时，要把设计稿的尺寸除以二。但这样的话，vent组件就会变得很小。
-      // 需求：vent样式按照37.5来转换；如果是设计稿的样式，按照75来转换。
+      // 但vant建议设置为37.5，因为vant是基于375px宽度的设计稿写的。据此，使用设计稿时，要把设计稿的尺寸除以二。但这样的话，vant组件就会变得很小。
+      // 需求：vant样式按照37.5来转换；如果是设计稿的样式，按照75来转换。
       // 解决方案： rootValues支持两种类型：
       // （1）数字：固定的数值
       // rootValue: 37.5,
@@ -23,7 +23,7 @@ module.exports = {
         // rootValue({ file }) {
         const { file } = obj
         // console.log(file)
-        // 如果file中含有vent样式，则按照37.5的rootValue来处理，否则75
+        // 如果file中含有vant样式，则按照37.5的rootValue来处理，否则75
         return file.indexOf('vant') !== -1 ? 37.5 : 75
       },
       propList: ['*']
